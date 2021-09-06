887a29715ea6c5375d9bd94404a6065e5a878f92
KOKEofKOKE <85344798+KOKEofKOKE@users.noreply.github.com>
2021-09-06 10:22:16 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 'use strict';
 
-function func() {
-  document.getElementById('info').textContent = '残念、まだ作成途中です';
-}
+let image2base64 = input_id => new Promise((resolve, reject) => {
+            let fileinput = document.getElementById(input_id);
+            if (fileinput == null)
+                  reject("cant find element");
+
+            let fileData = icon.files[0]
+            if (fileData === undefined)
+                  reject("no file")
+
+            if (!fileData.type.match('image.*'))
+                reject("file type error");
+
+            let reader = new FileReader();
+            reader.onload = () => {
+                resolve(reader.result)
+            }
+            reader.readAsDataURL(fileData);
+        })
+        let func = async () => {
+            let b64 = await image2base64('icon')
+            console.log(b64)
+            document.getElementById('info').textContent = '残念、まだ作成途中です';
+
+            //document.getElementById("result").innerText = b64;
+            //document.getElementById("result2").src = b64;
+        };
